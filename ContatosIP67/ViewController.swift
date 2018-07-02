582b8fb0ae6282d10748792967650f34399d935b
ios7649 <ios7649@users-macintosh.local>
2018-07-02 10:26:41 -0300

    Initial Commit

---
+++ ContatosIP67/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContatosIP67
+//
+//  Created by ios7649 on 7/2/18.
+//  Copyright © 2018 ios7649. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
