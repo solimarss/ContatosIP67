264becef2439d6d8b35f8733354042c4ba66c0d7
ios7649 <ios7649@users-macintosh.local>
2018-07-03 17:54:50 -0300

    extencao de String para i18n

---
+++ ContatosIP67/String+Lacalized.swift
@@ -0,0 +1,17 @@
+//
+//  String+Lacalized.swift
+//  ContatosIP67
+//
+//  Created by ios7649 on 7/3/18.
+//  Copyright © 2018 ios7649. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    
+    var localized: String {
+        return  NSLocalizedString(self, comment: "")
+    }
+    
+}
